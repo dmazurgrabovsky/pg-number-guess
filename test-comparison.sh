b1582eac80795b6b93d02cd2ce0d7a115c44b85b
dmazurgrabovsky <d.mazurgrabovsky@gmail.com>
2024-12-30 11:24:59 +0100

    test: add sample with (( )) vs [[ ]] producing the same result

--- test-comparison.sh
+++ test-comparison.sh
@@ -6,15 +6,30 @@ echo $RANRANDOM_NUMBER
 
 TEST_VALUES=( "1" "2" "3")  
 
+# test [[ -]] comparison
 for NEXT_NO in "${TEST_VALUES[@]}"; 
 do
-    if [[ $NEXT_NO -gt $RANDOM_NUMBER ]] 
+    if [[ $NEXT_NO -gt $RANDOM_NUMBER ]]
     then 
         echo "It's higher than that, guess again:"
-    elif [[ $NEXT_NO -lt $RANDOM_NUMBER ]] 
+    elif [[ $NEXT_NO < $RANDOM_NUMBER ]]
     then
         echo "It's lower than that, guess again:"
     else
         echo "You guessed it "
     fi
 done
+
+# vs arithmetic (()) comparison
+# for NEXT_NO in "${TEST_VALUES[@]}"; 
+# do
+#     if (( NEXT_NO > RANDOM_NUMBER ))
+#     then 
+#         echo "It's higher than that, guess again:"
+#     elif (( NEXT_NO < RANDOM_NUMBER ))
+#     then
+#         echo "It's lower than that, guess again:"
+#     else
+#         echo "You guessed it "
+#     fi
+# done
